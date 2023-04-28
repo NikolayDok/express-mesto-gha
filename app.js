e7c5ee11d7c685186bb09ea3f2eb7e465f8133eb
Nikolay Dokukin <frostphoto@yandex.ru>
2023-04-28 13:09:27 +0300

    fix: error in tests

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const NotFoundError = require('./errors/NotFoundError');
 const handleErrorDefault = require('./middlewares/handleErrorDefault');
 
 const router = require('./routes/index');
