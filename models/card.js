af587a362c1399d23b1b64a9f835f5c93cfed97b
Nikolay Dokukin <frostphoto@yandex.ru>
2023-05-14 23:33:38 +0300

    fixed errors

--- models/card.js
+++ models/card.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const regExUrl = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
+
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -10,6 +12,9 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
+    validate: {
+      validator: (v) => regExUrl.test(v),
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
